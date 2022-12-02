cee09619e1ccfdd6c75d8aa14023dcd0a57e5aaa
Nilesh <nilesh@haachico.com>
2022-12-02 09:37:52 +0530

    done style and fix bugs

--- script.js
+++ script.js
@@ -3,47 +3,55 @@ const qtyInput = document.querySelector("#qty-input");
 const currInput = document.querySelector("#curr-input");
 const submitButton = document.querySelector(".submit-btn");
 const output = document.querySelector(".output");
+const body = document.querySelector("body");
 
 function calcProfitAndLoss(initialPrice, qtyOfStock, currPrice) {
   if (initialPrice > currPrice) {
     const loss = (initialPrice - currPrice) * qtyOfStock;
     const lossPercent = (loss / initialPrice) * 100;
-    const message = `Hey you have made a loss of Rs ${loss} and your loss percentage is ${lossPercent}%`;
+    body.style.backgroundColor = "#991b1b";
+    let message = `Hey you have made a loss of Rs ${loss} and your loss percentage is ${lossPercent}%.`;
 
     return message;
   } else if (currPrice > initialPrice) {
     const profit = (currPrice - initialPrice) * qtyOfStock;
     const proftPercent = (profit / initialPrice) * 100;
-    const message = `Yay! you have made a profit of Rs ${profit} and your profit percentage is ${proftPercent}%`;
+    body.style.backgroundColor = "#065f46";
+    let message = `Yay! you have made a profit of Rs ${profit} and your profit percentage is ${proftPercent}%.`;
 
     return message;
   } else {
-    const message = `You have neither made a profit nor a loss 😐`;
+    let message = `You have neither made a profit nor a loss 😐.`;
 
     return message;
   }
 }
 
 function clickHandler() {
-  //Retrieve values from the input box
   const initialPrice = Number(initialInput.value);
   const qtyOfStock = Number(qtyInput.value);
   const currPrice = Number(currInput.value);
-  const result = calcProfitAndLoss(initialPrice, qtyOfStock, currPrice);
 
-  output.innerText = result;
+  if (initialPrice > 0 && qtyOfStock > 0 && currPrice > 0) {
+    const result = calcProfitAndLoss(initialPrice, qtyOfStock, currPrice);
+
+    output.innerText = result;
+  } else {
+    output.style.color = "white";
+    body.style.backgroundColor = "hsla(12, 6%, 15%, 0.758)";
+    output.innerText = `*Please enter all fields!`;
+  }
 }
 
-// calcProfitAndLoss(10, 10, 20);
+// function clickHandler() {
+//   const initialPrice = Number(initialInput.value);
+//   const qtyOfStock = Number(qtyInput.value);
+//   const currPrice = Number(currInput.value);
+
+//     const result = calcProfitAndLoss(initialPrice, qtyOfStock, currPrice);
+
+//     output.innerText = result;
+
+// }
+
 submitButton.addEventListener("click", clickHandler);
-// // do we need to pass arguments here?
-
-function sumOfValues(a, b) {
-  const sumVal = a + b;
-  return sumVal;
-}
-
-function mulOfVal(a, b) {
-  const val = a * b;
-  return val;
-}
